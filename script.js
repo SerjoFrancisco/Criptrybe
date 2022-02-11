1b081b96521cf6a3a375de98f3e5d39be212b2da
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-02-11 19:30:22 -0300

    Implementação da função que faz aparecer a lista de cryptos e a mensagem de carregamento

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+const cryptoList = document.getElementById('crypto-list');
+
+const createElement = (tag, ...classNames) => {
+  const el = document.createElement(tag);
+  if (classNames.length === 0) return el;
+  el.className = classNames.join(' ');
+  return el;
+}
+
+const listCrypto = async () => {
+  const carregando = createElement('div', 'carregando');
+  carregando.innerText = 'Carregando...';
+  cryptoList.appendChild(carregando);
+  const list = await fetchCryptoList();
+  document.querySelector('.carregando').remove();
+  list.forEach(({ symbol, price }) => {
+    const text = `${symbol} Price: ${price}`;
+    const li = createElement('li');
+    li.innerText = text;
+    cryptoList.appendChild(li);
+  });
+}
+
 window.onload = async () => {
-  fetchCryptoList();
+  await listCrypto();
+  // await fetchCryptoList();
   console.log(await fetchCrypto('BTCBRL'));
 }
