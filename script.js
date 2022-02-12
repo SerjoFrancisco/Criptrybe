ac56b6c0fdb5519a371b78cdb5a3fb39587dbe60
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-02-11 22:19:09 -0300

    FIX: Adicionando o tracinho na lista de criptos

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const listCrypto = async () => {
   const list = await fetchCryptoList();
   document.querySelector('.carregando').remove();
   list.forEach(({ symbol, price }) => {
-    const text = `${symbol.substring(0, symbol.length -3)} ${parseFloat(price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+    const text = `${symbol.substring(0, symbol.length -3)} - ${parseFloat(price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
     const li = createElement('li', 'item-list');
     li.innerText = text;
     cryptoList.appendChild(li);
