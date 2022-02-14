4333853f325ad7ac2feb4a28dbc61b98e24faa9c
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-02-13 23:29:28 -0300

    Implementei a função de compra mas falta tratar o erros

--- cryptoMovement.js
+++ cryptoMovement.js
@@ -1,24 +1,22 @@
+// const obj2 = JSON.parse(localStorage.getItem(user));
+// obj2.funds = 100000;
+// localStorage.setItem(user, JSON.stringify(obj2));
 const buyCrypto = async () => {
   const user = document.querySelector('.user-text span').id;
-  // const obj = JSON.parse(localStorage.getItem(user));
-  // obj.funds = 100000;
-  // localStorage.setItem(user, JSON.stringify(obj));
   const symbol = document.querySelector('#options').value;
   const cryptoList = await fetchCryptoList();
   const price = parseFloat(cryptoList.find(({ symbol: sy }) => sy === symbol).price);
   const valueInput = parseFloat(document.getElementById('input-value').value);
-  
-  const saldo = JSON.parse(localStorage.getItem('user')).funds;
-  console.log(saldo);
-
-  // if ( saldo >= valueInput) {
-  //   localStorage.getItem('')
-  //   localStorage.setItem('saldo', saldo - valueInput);
-  //   console.log(saldo);
-  //   const result = valueInput / price;
-  //   console.log(result.toFixed(8));
-  //   return result.toFixed(8);
-  // }
+  const obj = JSON.parse(localStorage.getItem(user));
+  let { funds, positions } = obj;
+  if ( funds >= valueInput) {
+    funds -= valueInput;
+    const result = valueInput / price;
+    !positions[symbol] ? positions[symbol] = result : positions[symbol] += result;
+    localStorage.setItem(user, JSON.stringify(obj));
+    console.log(obj);
+    return result.toFixed(8);
+  }
 }
 
 // const sellCrypto = async (symbol) => {
